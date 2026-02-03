6576823933b4245695972b070f7bf36e582808b2
Soumyadeep Mishra <soumyadeepmishra14@gmail.com>
2026-02-03 19:37:44 +0530

    changes made for deployment

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,15 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const BACKEND_URL = axios.create ({
-    baseURL : "http://localhost:4001/api/v1/noteapp/"
-})
+// const BACKEND_URL = axios.create ({
+//     baseURL : "http://localhost:4001/api/v1/noteapp/"
+// })
+
+// export default BACKEND_URL;
+
+import axios from "axios";
+
+const BACKEND_URL = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL
+});
 
 export default BACKEND_URL;
